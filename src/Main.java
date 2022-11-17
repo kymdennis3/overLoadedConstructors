5cfc4faf72bfb4c8c7315efb1378d758c5a6ea92
kymdennis3 <kymdennis3@gmail.com>
2022-11-17 11:19:45 +0300

    called all the attributes of our constructor

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class  Main {
         Pizza pizza = new Pizza("thick crust", "tomato", "Mozzarella","Pepperoni");
 
         System.out.println("This are the ingredients to our Pizza");
-        System.out.println();
+        System.out.println(); // creates a space between our lines
         System.out.println(pizza.bread);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.topping);
 
     }
 }
